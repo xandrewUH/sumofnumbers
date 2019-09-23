04bc4e31b6d2635226cc30365f561a3e6e766d82
xandrewUH <xandrew@hawaii.edu>
2019-09-22 15:57:38 -1000

    E26

---
+++ sumofnumbers.js
@@ -0,0 +1,38 @@
+const numsList = [2, 4, 6, 8];
+
+function sumFor(nums) {
+  let total = 0;
+  for (const num of nums) {
+    total += num;
+  }
+  return total;
+}
+
+console.log(sumFor(numsList));
+
+function sumWhile(nums) {
+  let total = 0;
+  let i = 0;
+  while (i < nums.length) {
+    total += nums[i];
+    i++;
+  }
+  return total;
+}
+
+console.log(sumWhile(numsList));
+
+function sumRecursion(nums) {
+  if (nums.length === 0) {
+    return 0;
+  }
+  return nums[0] + sumRecursion(nums.slice(1, nums.length));
+}
+
+console.log(sumRecursion(numsList));
+
+function sumTheSimpleWay(nums) {
+  return _.reduce(nums, function (memo, num) { return memo + num; }, 0);
+}
+
+console.log(sumTheSimpleWay(numsList));
